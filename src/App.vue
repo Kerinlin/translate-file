c4326de3ba5b34fa835d93cc2eaadf9693235d77
kerinlin <18296613547@qq.com>
2020-11-02 10:14:06 +0800

    Change style and add auto update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
   <div id="app">
+    <header class="header-container">
+      <i @click="minimize" class="fa fa-minus" aria-hidden="true"></i>
+      <i @click="close" class="fa fa-times" aria-hidden="true"></i>
+    </header>
     <router-view />
   </div>
 </template>
+
+<script>
+const { ipcRenderer } = require("electron");
+export default {
+  methods: {
+    close() {
+      ipcRenderer.send("close");
+    },
+    minimize() {
+      ipcRenderer.send("minimize");
+    }
+  }
+};
+</script>
 
 <style lang="less" scoped>
 #app {
@@ -12,8 +30,25 @@
   background-image: url("./assets/bg1.png");
   background-size: cover;
   // background-repeat: no-repeat;
-  &:hover {
-    cursor: url("./assets/defaultcur.png"), default;
+  .header-container {
+    width: 100%;
+    text-align: right;
+    i {
+      padding: 5px 10px;
+      border-radius: 50%;
+      background-color: transparent;
+      &:hover {
+        cursor: pointer;
+        background: #413f4c;
+      }
+    }
+    .fa {
+      -webkit-app-region: no-drag;
+      margin: 5px 5px 0px 0px;
+      font-size: 28px;
+      color: #fff;
+      font-weight: 200;
+    }
   }
 }
 </style>
